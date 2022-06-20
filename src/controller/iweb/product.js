b86e6996d0d8e71263c5ab05d9f635250305e906
Jimmy <1064425721@qq.com>
2022-06-20 19:11:37 +0800

    ci:添加pm2 restart

--- src/controller/iweb/product.js
+++ src/controller/iweb/product.js
@@ -14,7 +14,6 @@ module.exports = {
                     for (let j = 0; j < len; j++) {
                         theData.ware_topicsTags.push(obj[keysArr[j]])
                     }
-
                     let qsTags = qs.stringify({ topicstags: theData.ware_topicsTags }, { arrayFormat: 'indices' })
                     let qsCommits = qs.stringify(theData.ware_commits, { arrayFormat: 'repeat' })
                     let qsLanguages = qs.stringify(theData.ware_languages, { arrayFormat: 'repeat' })
